1f6ca94dc78b8984743dee585b02a9d855d583b6
Boldmax <olabode.adewunmi@mkobo.com.ng>
2021-09-30 20:08:54 +0100

    Completed

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require("express");
 const productRoutes = require('./routes/productRoutes');
 const connectDB = require('./config/db');
+const path =  require('path');
 
 
 connectDB();
@@ -19,6 +20,10 @@ const PORT = process.env.PORT || 5001;
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
 };
 
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
